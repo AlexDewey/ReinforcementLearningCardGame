e290f78657c4606383b310c342a363568a536426
Alex Dewey <alexmarcdewey@gmail.com>
2022-06-19 11:35:24 -0700

    Finished board_input for ai to view all visible game data

--- player.py
+++ player.py
@@ -20,31 +20,43 @@ class Player:
             else:
                 canPass = True
 
-        # 12 values, 4 rows, 5 columns = binary representation: [0-11], [0-3], [0,4] = 12x4x5 = 240
+        # 4 rows [0-3], 5 columns [0-4], 13 values [0-12], 4 x 5 x 13 = 260
         self_board = np.zeros(260).reshape(-1, 1)
         if playerNum == 1:
             for row_index, row in enumerate(board.p1_rows):
                 for column_index, card_value in enumerate(row):
-                    print(str(card_value))
                     self_board[row_index + (4 * column_index) + (20 * card_value) ] = 1
         else:
             for row_index, row in enumerate(board.p2_rows):
                 for column_index, card_value in enumerate(row):
-                    print(str(card_value))
                     self_board[row_index + (4 * column_index) + (20 * card_value)] = 1
 
-        print(self_board.tolist())
-
-        # 12 values, 4 rows, 5 columns = binary representation: [0-11], [0-3], [0,4] = 12x4x5 = 240
+        # 4 rows [0-3], 5 columns [0-4], 13 values [0-12], 4 x 5 x 13 = 260
         opponenet_board = np.zeros(260).reshape(-1, 1)
+        if playerNum == 1:
+            for row_index, row in enumerate(board.p2_rows):
+                for column_index, card_value in enumerate(row):
+                    opponenet_board[row_index + (4 * column_index) + (20 * card_value) ] = 1
+        else:
+            for row_index, row in enumerate(board.p1_rows):
+                for column_index, card_value in enumerate(row):
+                    opponenet_board[row_index + (4 * column_index) + (20 * card_value)] = 1
 
-        # 5 cards held x 12 values = binary representation: [0-4], [0-11] = 5x12 = 60
-        hand = np.zeros(260).reshape(-1, 1)
-
-        # 5 cards held by player = 5 cards counted [0-4] = 5
-        self_counted = np.zeros(5).reshape(-1, 1)
+        # 5 cards held [0-4], 13 values [0-12], 5 x 13 = 65
+        hand = np.zeros(65).reshape(-1, 1)
+        if playerNum == 1:
+            for hand_index, card_value in enumerate(board.p1_hand):
+                hand[hand_index + (5 * card_value)] = 1
+        else:
+            for hand_index, card_value in enumerate(board.p2_hand):
+                hand[hand_index + (5 * card_value)] = 1
 
         # 5 cards held by opponent = 5 cards counted [0-4] = 5
         opponent_counted = np.zeros(5).reshape(-1, 1)
+        if playerNum == 1:
+            opponent_counted[len(board.p2_hand)] = 1
+        else:
+            opponent_counted[len(board.p1_hand)] = 1
 
-        # In total 550
+        # 590 Total Length
+        board_input = np.concatenate((np.concatenate((np.concatenate((self_board, opponenet_board)), hand)), opponent_counted))
