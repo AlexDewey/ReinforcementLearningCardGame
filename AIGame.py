8222e19afc0eafc83917c900f0e1c37de3a49a24
Alex Dewey <alexmarcdewey@gmail.com>
2022-06-30 21:37:02 -0700

    SavedModels Implementation with custom mutation and generations

--- AIGame.py
+++ AIGame.py
@@ -1,8 +1,6 @@
-import math
-from time import sleep
+from tensorflow import keras
 
 from board import *
-from gameView import *
 from player import *
 
 
@@ -32,26 +30,12 @@ def game_view(board):
     print("\n\n\n\n")
 
 
-def model_mutate(weights):
-    # mutate each models weights
-    for i in range(len(weights)):
-        for j in range(len(weights[i])):
-            if random.uniform(0, 1) > .85:
-                change = random.uniform(-.5,.5)
-                weights[i][j] += change
-    return weights
-
-
 def create_model():
-
-    model = Sequential()
-    model.add(Dense(590, input_shape=(590,)))
-    model.add(Activation('relu'))
-    model.add(Dense(590 * 2, input_shape=(590,)))
-    model.add(Activation('relu'))
-    # 100 for placement and card in defence, 5 for attack and 1 for pass. = 106
-    model.add(Dense(106, input_shape=(590,)))
-    model.add(Activation('sigmoid'))
+    model = keras.Sequential([
+        keras.layers.Dense(590, input_shape=(590,)),
+        keras.layers.Dense((590 * 2), activation="relu"),
+        keras.layers.Dense(106, activation="relu")
+    ])
 
     model.compile(loss='mse', optimizer='adam')
 
@@ -60,54 +44,58 @@ def create_model():
 
 class KerduGame:
 
-    def __init__(self):
-        print("Generating initial population")
+    def __init__(self, mutation_rate, model_nums, generations):
+        self.mutation_rate = mutation_rate
         CREATE_NEW_POOL = True
-        TOTAL_MODELS = 50 # Even number please!
-        GENERATIONS = 10
-        current_pool = list()
+        TOTAL_MODELS = model_nums  # Even number please!
+        GENERATIONS = generations
+        base_pool = list()
 
         for i in range(TOTAL_MODELS):
             model = create_model()
-            current_pool.append(model)
+            model.save_weights("SavedModels/model_base_" + str(i) + ".keras")
+            base_pool.append(model)
 
         if not CREATE_NEW_POOL:
             for i in range(TOTAL_MODELS):
-                current_pool[i].load_weights("SavedModels/model_new"+str(i)+".keras")
+                base_pool[i].load_weights("SavedModels/model_new" + str(i) + ".keras")
+
 
         for generation in range(GENERATIONS):
-            print("Computing fitness")
+            print("Generation " + str(generation) + "/" + str(generations) + ", mutation rate " + str(self.mutation_rate))
 
-            random.shuffle(current_pool)
-            p1_pool = current_pool[0:math.floor(len(current_pool)/2)]
-            p2_pool = current_pool[math.floor(len(current_pool)/2):]
+            random.shuffle(base_pool)
+            p1_pool = base_pool[0:math.floor(len(base_pool) / 2)]
+            p2_pool = base_pool[math.floor(len(base_pool) / 2):]
 
             winnerIdx = list()
 
-            for player in range(len(p1_pool)):
+            for player_index in range(len(p1_pool)):
                 p1_win = False
                 p2_win = False
 
+                # rematches = 0
+
                 while (p1_win and p2_win) or (not p1_win and not p2_win):
                     board = Board()
-                    [p1_win, p2_win] = self.play_game(board, p1_pool[player], p2_pool[player])
+                    [p1_win, p2_win] = self.play_game(board, p1_pool[player_index], p2_pool[player_index])
+                    # rematches += 1
 
                 if p1_win:
                     winnerIdx.append(1)
-                    print("1")
+                    # print("1    " + str(rematches))
                 else:
                     winnerIdx.append(2)
-                    print("2")
+                    # print("2    " + str(rematches))
 
-            print("Selection")
+            # print("Selection")
             # P1 Pool are the pools that survive
             for idx in winnerIdx:
                 if idx == 2:
                     p1_pool[idx] = p2_pool[idx]
 
-            print("Crossover")
             # Crossover self and another random indexed model
-            for parent1 in p1_pool:
+            for parent_index, parent1 in enumerate(p1_pool):
                 rand_idx = math.floor(random.random() * len(p1_pool))
                 parent2 = p1_pool[rand_idx]
 
@@ -117,25 +105,60 @@ class KerduGame:
                 new_weight1 = weight1
                 new_weight2 = weight2
 
-                gene = math.floor(random.random() * len(new_weight1))
+                for layer_index, layer in enumerate(new_weight1):
+                    for gene_index in range(len(layer)):
+                        if random.random() > 0.85:
+                            layer[gene_index] = weight2[layer_index][gene_index]
+                for layer_index, layer in enumerate(new_weight2):
+                    for gene_index in range(len(layer)):
+                        if random.random() > 0.85:
+                            layer[gene_index] = weight2[layer_index][gene_index]
 
-                new_weight1[gene] = weight2[gene]
-                new_weight2[gene] = weight1[gene]
+                # Mutation
+                for layer in new_weight1:
+                    for gene_index in range(len(layer)):
+                        if random.random() > 1 - self.mutation_rate:
+                            layer[gene_index] += random.uniform(-0.5, 0.5)
+                for layer in new_weight2:
+                    for gene_index in range(len(layer)):
+                        if random.random() > 1 - self.mutation_rate:
+                            layer[gene_index] += random.uniform(-0.5, 0.5)
 
-                cross_over_weights = np.asarray([new_weight1, new_weight2])
 
-                mutated1 = model_mutate(cross_over_weights[0])
-                mutated2 = model_mutate(cross_over_weights[0])
+                # model.set_weights
+                # Set values to location
+                parent1.set_weights(new_weight1)
+                p2_pool[parent_index] = create_model().set_weights(new_weight2)
 
-                new_weights = list()
-                new_weights.append(mutated1)
-                new_weights.append(mutated2)
+            base_pool = p1_pool + p2_pool
 
-            print("Mutation")
+        original_pool = list()
+        for i in range(TOTAL_MODELS):
+            model = create_model()
+            original_pool.append(model.load_weights("SavedModels/model_base_"+str(i)+".keras"))
 
-        print("Stop after n generations")
+        trained_pool = base_pool
 
-    def play_game(self, board, p1_model, p2_model):
+        for pool_index in range(len(trained_pool)):
+            p_trained_win = False
+            p_base_win = False
+
+            rematches = 0
+
+            while (p_trained_win and p_base_win) or (not p_trained_win and not p_base_win):
+                board = Board()
+                [p_trained_win, p_base_win] = self.play_game(board, trained_pool[pool_index], p_base_win[pool_index])
+                rematches += 1
+
+            if p_trained_win:
+                print("1    " + str(rematches))
+            else:
+                print("2    " + str(rematches))
+
+
+
+    @staticmethod
+    def play_game(board, p1_model, p2_model):
 
         p1 = Player(1, p1_model)
         p2 = Player(2, p2_model)
@@ -148,6 +171,8 @@ class KerduGame:
 
         while board.gameOver is False:
             # Update game board
+
+            # game view
             # game_view(board)
 
             # If both players passed, draw cards. Automatically the case at the start of the game
@@ -177,7 +202,8 @@ class KerduGame:
                     card_in_play = True
 
             # actions: ["pass", ["attack", cardHandIndex], ["defend", cardTargetedRow, cardTargetedColumn, cardUsedIdx]]
-            action = players[playerNum - 1].get_player_action(board, playerNum, players[playerNum - 1].model, card_in_play)
+            action = players[playerNum - 1].get_player_action(board, playerNum, players[playerNum - 1].model,
+                                                              card_in_play)
 
             if action != "pass":
                 playerPass[playerNum - 1] = False
@@ -208,7 +234,8 @@ class KerduGame:
         if len(board.p1_rows[0]) != 0:
             win_return[1] = True
 
-        return  win_return
+        return win_return
 
 
-KerduGame()
+KerduGame(0.01, 10, 100)
+print("Done!")
