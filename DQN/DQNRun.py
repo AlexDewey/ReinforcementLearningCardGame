7ff58971888958aecc8380bcec6ab3bc4c29030f
Alex Dewey <alexmarcdewey@gmail.com>
2022-07-11 14:40:43 -0700

    Restructuring from basics and following tutorial

--- DQN/DQNRun.py
+++ DQN/DQNRun.py
@@ -1,99 +1,50 @@
-# Actor and critic network
-from DQN import Environment
+import numpy as np
 
-actor_net = ActorDistributionNetwork(action_spec)
-critic_net = CriticNetwork((observation_spec, action_spec), ...)
+import tensorflow as tf
 
-# Create a Soft-Actor-Critic Agent
-tf_agent = sac_agent.SacAgent(
-    critic_network=critic_network,
-    actor_network=actor_netwrok,
-    actor_optimizer=AdamOptimizer(learning_rate=0.001),
-    critic_optimizer=AdamOptimizer(learning_rate=0.001))
-
-# Get experience and train
-dataset = replay_buffer.as_dataset(num_steps=2).prefetch(3)
-for batched_experience in dataset:
-    tf_agent.train(batched_experience)
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
 
 
+# Hyperparameters
 
-# ---------------------
+num_iterations = 20000
 
-# Single
+initial_collect_steps = 100
+collect_steps_per_iteration = 1
+replay_buffer_max_length = 100000
 
-# # Defining the Environment
-# env = Environment.BreakoutEnv()
-# # Define a Policy (How the agent plays)
-# policy = MyPolicy()
-#
-# time_step = env.reset()
-# episode_return = 0
-#
-# while not time_step.is_last():
-#     policy_step = policy.action(time_step)
-#     time_step = env.step(policy_step.action)
-#     episode_return += time_step.reward
+batch_size = 64
+learning_rate = 0.001
+log_interval = 200
 
+num_eval_episodes = 10
+eval_interval = 1000
 
+# Environment
 
-# Parallel
+env_name = 'CartPole-v0'
+env = suite_gym.load(env_name)
 
-# Running the game 4 times
-parallel_env = ParallelPyEnvironment([BreakoutEnv() for _ in range(4)])
-tf_env = TFPyEnvironment(parallel_env)
+env.reset()
 
-time_step = tf_env.reset()
-episode_return = tf.zeros([4])
-for _ in range(num_steps):
-    policy_step = policy.action(time_step)
-    time_step = tf_env.step(policy_step.aciton)
-    episode_return += time_step.reward
+# Obs Spec
 
+# Reward Spec
 
+# Action Spec
 
+time_step = env.reset()
 
-# -----------------------
+action = np.array(1, dtype=np.int32)
 
+next_time_step = env.step(action)
 
-# Creating an agent
-agent = DqnAgent(q_network=..., optimizer=...)
+train_py_env = suite_gym.load(env_name)
+eval_py_env = suite_gym.load(env_name)
 
-# Policy used to explore & collect training data
-collect_policy = agent.collect_policy
-
-# Train from a batch of experience
-loss_info = agent.train(batched_experience=trajectories)
-
-# The deployment policy
-deployment_policy = agent.policy
-
-
-
-
-# --------------------------
-
-# Training neural network
-
-# Create a Network
-q_net = q_network.QNetwork(observation_spec, action_spec, ...)
-
-# Create the Agent
-agent = dqn_agent.DqnAgent(q_network=q_net,
-                           optimizer=AdamOptimizer(learning_rate=0.001))
-
-# Get experience and train the Agent
-dataset = replay_buffer.as_dataset(num_steps=2).prefetch(3)
-
-for batched_experience in dataset:
-    agent_train(batched_experience)
-
-
-
-
-
-
-
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 
 
 
