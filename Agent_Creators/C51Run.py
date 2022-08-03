b76859d38a71f64aa629d9da1d0863fc7aac55ab
Alex Dewey <alexmarcdewey@gmail.com>
2022-08-02 19:36:01 -0700

    Reorganization of C51 into a class

--- Agent_Creators/C51Run.py
+++ Agent_Creators/C51Run.py
@@ -35,145 +35,139 @@ def compute_avg_return(environment, policy, num_episodes=300):
     return avg_return.numpy()[0]
 
 
-def train():
-    # Number of iterations during training, 5000000 was used with the original paper to show results
-    num_iterations = 100000
+class C51():
 
-    # Initial collection for batch
-    initial_collect_steps = 1000
-    collect_steps_per_iteration = 1
-    # How many elements can be stored in the replay buffer
-    replay_buffer_capacity = 100000
+    # train_time: (int) number of iterations
+    # fc_layer_params: (int) size of fully connected layer
+    # min_q: (int) minimum value of q that can be rewarded
+    # max_q: (int) maximum value of q that can be rewarded
+    # steps_considered: (int) how far into the future are we counting reward values
+    def __init__(self, train_time, fc_layer_params, min_q, max_q, steps_considered):
+        # Number of iterations during training, 5000000 was used with the original paper to show results
+        self.num_iterations = train_time  # 100000
 
-    #
-    fc_layer_params = (242,)
+        # Initial collection for batch
+        self.initial_collect_steps = 1000
+        self.collect_steps_per_iteration = 1
+        # How many elements can be stored in the replay buffer
+        self.replay_buffer_capacity = 100000
 
-    # C51 Learning hyperparameters
-    batch_size = 64
-    learning_rate = 0.01
-    gamma = 0.99
-    log_interval = 200
+        #
+        self.fc_layer_params = (fc_layer_params,)  # 242
 
-    # Number of atoms to approximate probability distributions, more the better
-    num_atoms = 51
-    # Values should be set to the min and max step rewards
-    min_q_value = -100
-    max_q_value = 100
-    # Computing error between current time step and next time step using 4 steps
-    n_step_update = 4
+        # C51 Learning hyperparameters
+        self.batch_size = 64
+        self.learning_rate = 0.01
+        self.gamma = 0.99
+        self.log_interval = 200
 
-    num_eval_episodes = 10
-    eval_interval = 1000
+        # Number of atoms to approximate probability distributions, more the better
+        self.num_atoms = 51
+        # Values should be set to the min and max step rewards
+        self.min_q_value = min_q  # -100
+        self.max_q_value = max_q  # 100
+        # Computing error between current time step and next time step using 4 steps
+        self.n_step_update = steps_considered
 
-    # Environments are wrapped in a time limit and then a tf environment wrapper.
-    train_py_env = wrappers.TimeLimit(KerduGameEnv(), duration=100)
-    eval_py_env = wrappers.TimeLimit(KerduGameEnv(), duration=100)
+        self.num_eval_episodes = 10
+        self.eval_interval = 1000
 
-    train_env = tf_py_environment.TFPyEnvironment(train_py_env)
-    eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+    def train(self, env):
 
-    # A categorical q network is required for our categorical C51
-    categorical_q_net = categorical_q_network.CategoricalQNetwork(
-        train_env.observation_spec(),
-        train_env.action_spec(),
-        num_atoms=num_atoms,
-        fc_layer_params=fc_layer_params)
+        # Environment setup
+        train_py_env = wrappers.TimeLimit(env(), duration=100)  # KerduGameEnv()
+        eval_py_env = wrappers.TimeLimit(env(), duration=100)  # KerduGameEnv()
 
-    # Adam optimizer used for optimization
-    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
-
-    # Track how many times the network was updated
-    train_step_counter = tf.Variable(0)
-
-    # The main difference between this and a vainilla C51 is the min/max q difference
-    agent = categorical_dqn_agent.CategoricalDqnAgent(
-        train_env.time_step_spec(),
-        train_env.action_spec(),
-        epsilon_greedy=0.1,
-        categorical_q_network=categorical_q_net,
-        optimizer=optimizer,
-        min_q_value=min_q_value,
-        max_q_value=max_q_value,
-        n_step_update=n_step_update,
-        td_errors_loss_fn=common.element_wise_squared_loss,
-        gamma=gamma,
-        train_step_counter=train_step_counter)
-
-    # Initialize C51
-    agent.initialize()
-
-    # Establishing random policy800
-    random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
-                                                    train_env.action_spec())
-
-    # Saves observations and action pairs for training. As to not bias the network to specific situations these are fed
-    # in somewhat randomly
-    replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-        data_spec=agent.collect_data_spec,
-        batch_size=train_env.batch_size,
-        max_length=replay_buffer_capacity)
-
-    # Observes the environment, gets an action, gets the resulting time step, then saves the result to the buffer
-    def collect_step(environment, policy):
-        time_step = environment.current_time_step()
-        action_step = policy.action(time_step)
-        next_time_step = environment.step(action_step.action)
-        traj = trajectory.from_transition(time_step, action_step, next_time_step)
-        replay_buffer.add_batch(traj)
-
-    # Undergoing initial steps to add to replay buffer before official training as it cannot start empty
-    for _ in range(initial_collect_steps):
-        collect_step(train_env, random_policy)
-
-    # The dataset takes our replay buffer and creates trajectories
-    dataset = replay_buffer.as_dataset(
-        num_parallel_calls=3, sample_batch_size=batch_size,
-        num_steps=n_step_update + 1, single_deterministic_pass=False).prefetch(3)
-
-    iterator = iter(dataset)
-
-    # (Optional) Optimize by wrapping some of the code in a graph using TF function.
-    agent.train = common.function(agent.train)
-
-    # Reset the train step
-    agent.train_step_counter.assign(0)
-
-    # Evaluate the agent's policy800 once before training.
-    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
-    returns = [avg_return]
-
-    # Actual training begins
-    for _ in range(num_iterations):
-
-        # Collect a few steps using default agent greedy policy800 and save to the replay buffer.
-        for _ in range(collect_steps_per_iteration):
-            collect_step(train_env, agent.collect_policy)
-
-        # Sample a batch of data from the buffer and update the agent's network.
-        experience, unused_info = next(iterator)
-        train_loss = agent.train(experience)
-
-        step = agent.train_step_counter.numpy()
-
-        # if step % log_interval == 0:
-        #     print('step = {0}: loss = {1}'.format(step, train_loss.loss))
-
-        if step % eval_interval == 0:
-            avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
-            print('step = {0}: Average Return = {1:.2f}'.format(step, avg_return))
-            returns.append(avg_return)
-
-    policy_dir = os.path.join('../SavedModels', 'policy242')
-    tf_policy_saver = policy_saver.PolicySaver(agent.policy)
-    tf_policy_saver.save(policy_dir)
-
-    steps = range(0, num_iterations + 1, eval_interval)
-    plt.plot(steps, returns)
-    plt.ylabel('Average Return')
-    plt.xlabel('Step')
-    plt.ylim(top=110)
-
-    plt.show()
+        train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+        eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 
 
-train()
+        # C51 setup
+        categorical_q_net = categorical_q_network.CategoricalQNetwork(
+            train_env.observation_spec(),
+            train_env.action_spec(),
+            num_atoms=self.num_atoms,
+            fc_layer_params=self.fc_layer_params)
+
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=self.learning_rate)
+        train_step_counter = tf.Variable(0)
+        agent = categorical_dqn_agent.CategoricalDqnAgent(
+            train_env.time_step_spec(),
+            train_env.action_spec(),
+            epsilon_greedy=0.1,
+            categorical_q_network=categorical_q_net,
+            optimizer=optimizer,
+            min_q_value=self.min_q_value,
+            max_q_value=self.max_q_value,
+            n_step_update=self.n_step_update,
+            td_errors_loss_fn=common.element_wise_squared_loss,
+            gamma=self.gamma,
+            train_step_counter=train_step_counter)
+
+        agent.initialize()
+
+
+        # Creating replay buffer and filling before training
+        random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
+                                                        train_env.action_spec())
+
+        replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+            data_spec=agent.collect_data_spec,
+            batch_size=train_env.batch_size,
+            max_length=self.replay_buffer_capacity)
+
+        def collect_step(environment, policy):
+            time_step = environment.current_time_step()
+            action_step = policy.action(time_step)
+            next_time_step = environment.step(action_step.action)
+            traj = trajectory.from_transition(time_step, action_step, next_time_step)
+            replay_buffer.add_batch(traj)
+
+        for _ in range(self.initial_collect_steps):
+            collect_step(train_env, random_policy)
+
+        dataset = replay_buffer.as_dataset(
+            num_parallel_calls=3, sample_batch_size=self.batch_size,
+            num_steps=self.n_step_update + 1, single_deterministic_pass=False).prefetch(3)
+
+        iterator = iter(dataset)
+        agent.train = common.function(agent.train)
+        agent.train_step_counter.assign(0)
+        avg_return = compute_avg_return(eval_env, agent.policy, self.num_eval_episodes)
+        returns = [avg_return]
+
+
+        # Training starts
+        for _ in range(self.num_iterations):
+
+            # Collect a few steps using default agent greedy policy800 and save to the replay buffer.
+            for _ in range(self.collect_steps_per_iteration):
+                collect_step(train_env, agent.collect_policy)
+
+            # Sample a batch of data from the buffer and update the agent's network.
+            experience, unused_info = next(iterator)
+            train_loss = agent.train(experience)
+
+            step = agent.train_step_counter.numpy()
+
+            # if step % log_interval == 0:
+            #     print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+
+            if step % self.eval_interval == 0:
+                avg_return = compute_avg_return(eval_env, agent.policy, self.num_eval_episodes)
+                print('step = {0}: Average Return = {1:.2f}'.format(step, avg_return))
+                returns.append(avg_return)
+
+        # policy_dir = os.path.join('../SavedModels', 'policy242')
+        # tf_policy_saver = policy_saver.PolicySaver(agent.policy)
+        # tf_policy_saver.save(policy_dir)
+
+        # checkpoint_dir = os.path.join('../SavedModels', 'checkpoint')
+
+        steps = range(0, self.num_iterations + 1, self.eval_interval)
+        plt.plot(steps, returns)
+        plt.ylabel('Average Return')
+        plt.xlabel('Step')
+        plt.ylim(top=110)
+
+        plt.show()
